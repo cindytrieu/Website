f1260f9b81f175ac0182c7f36f0b62a771b48f23
Cindy Trieu <cindytrieu@email.arizona.edu>
2017-07-04 16:58:29 -0700

    adding stuff to temp branch for merging

---
+++ html/js/main.js
@@ -0,0 +1,32 @@
+/*
+	MAIN.JS
+	Author: Cindy Trieu
+*/
+
+function modify(el, mod){
+	var counter = el.parentNode.getElementsByClassName("counterVal")[0];
+	var val = Number.parseInt(counter.innerText);
+	val = val + mod;
+	counter.innerHTML = val;
+}
+
+function setVal(el) {
+	var val = Number.parseInt(prompt("Enter a new value", "0"));
+	if(val == null || isNaN(val) ) {
+		val = el.innerHTML;
+	}
+	el.innerHTML = val;
+}
+
+function changeName(el) {
+	var name = prompt("Enter new name", "name");
+	if(name.length == 0) {
+		name = el.innerHTML;
+	}
+	el.innerHTML = name;
+}
+
+function createCounter(container, el) {
+	var clone = container.firstElementChild.cloneNode(true);
+	container.insertBefore(clone, el);
+}
